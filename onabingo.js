70eb5b4432f5b7f5ae3df70c7b9cb729edf15cf1
Justin Myers <justin@justinmyers.net>
2014-09-21 23:07:17 -0400

    Generate random 5x5 board on load
    Still needs styling, of course.

--- onabingo.js
+++ onabingo.js
@@ -61,7 +61,31 @@ var BingoCard,
 BingoSpace = Backbone.Model.extend({});
 
 BingoCard = Backbone.Collection.extend({
-  model: BingoSpace
+  model: BingoSpace,
+  initialize: function( models, options ) {
+    this.cardHeight = options.cardHeight;
+    this.cardWidth = options.cardWidth;
+
+    this.on( 'reset', this.arrangeRows, this );
+  },
+  arrangeRows: function() {
+    var byRow = [],
+      cardHeight = this.cardHeight,
+      cardWidth = this.cardWidth;
+
+    // Keep track of the models by coordinates.
+    _.forEach( _.range( cardHeight ), function( rowIndex ) {
+      var row = [];
+
+      _.forEach( _.range( cardWidth ), function( colIndex ) {
+        row.push( this.at( rowIndex * cardWidth + colIndex ) );
+      }, this );
+
+      byRow.push( row );
+    }, this );
+
+    this.byRow = byRow;
+  }
 });
 
 /*****************************************************************************
@@ -70,6 +94,80 @@ BingoCard = Backbone.Collection.extend({
 
 var BingoCardView,
   BingoSpaceView;
+
+BingoSpaceView = Backbone.View.extend({
+  tagName: 'td',
+  className: 'bingo-space',
+  render: function() {
+    /**
+      * Renders the current view.
+      *
+      * @returns {BingoSpaceView}
+      */
+
+    var withHyphens = this.model.get( 'withHyphens' );
+
+    if ( withHyphens != null ) {
+      this.$el.text( withHyphens.join( '\u00ad' ) );
+    } else {
+      this.$el.text( this.model.get( 'word' ) );
+    }
+
+    return this;
+  }
+});
+
+BingoCardView = Backbone.View.extend({
+  tagName: 'table',
+  className: 'bingo-card',
+  initialize: function( options ) {
+    this.spaceViews = [];
+  },
+  render: function() {
+    /**
+      * Renders the current view.
+      *
+      * @returns {BingoCardView}
+      */
+
+    var byRow,
+      cardHeight,
+      cardWidth,
+      collection = this.collection;
+
+    cardHeight = collection.cardHeight;
+    cardWidth = collection.cardWidth;
+    byRow = collection.byRow;
+
+    // Empty this view.
+    this.$el.empty();
+
+    // Initialize a view for each BingoSpace.
+    this.spaceViews = _.map( byRow, function( rowModels ) {
+      return _.map( rowModels, function( spaceModel ) {
+        return new BingoSpaceView({
+          model: spaceModel
+        });
+      }, this );
+    }, this );
+
+    // Iterate over all of them again and build the DOM structure for the bingo
+    // card. (Yes, I could've done this all at once while I was initializing
+    // them earlier, but this is a bit easier to read, and I'm not particularly
+    // worried about performance with a typical size of bingo card.)
+    _.forEach( this.spaceViews, function( rowViews ) {
+      var $row = $( '<tr></tr>' );
+
+      _.forEach( rowViews, function( spaceView ) {
+        spaceView.render().$el.appendTo( $row );
+      }, this );
+
+      $row.appendTo( this.$el );
+    }, this );
+
+    return this;
+  }
+});
 
 /*****************************************************************************
   * INITIALIZATION
@@ -80,9 +178,24 @@ $( window.document ).ready(function() {
     cardView,
     words;
 
-  words = getWords( config.cardHeight * config.cardWidth, config.wordList );
+  words = getWords(
+    config.cardHeight * config.cardWidth,
+    config.wordList );
 
-  card = new BingoCard( words );
+  card = new BingoCard( null, {
+    cardHeight: config.cardHeight,
+    cardWidth: config.cardWidth
+  });
+  card.reset( words );
+  cardView = new BingoCardView({
+    collection: card,
+    el: $( '.bingo-card' )
+  });
+
+  cardView.render();
+
+  window.card = card;
+  window.cardView = cardView;
 });
 
 
@@ -93,17 +206,20 @@ $( window.document ).ready(function() {
 function getWords( wordCount, wordList ) {
   /**
     * Choose some number of random words (without duplicates) from a given
-    * list.
-    *
-    * Works with any array of objects, I suppose, but it's intended here for
-    * objects of words for bingo spaces.
+    * list, with a free space in the middle.
     *
     * @argument {number} wordCount - how many words to select
     * @argument {array} wordList - the list of words from which to choose
     * @returns {array}
     */
 
-  return _.shuffle( wordList ).slice( 0, wordCount );
+  var selectedWords = _.shuffle( wordList ).slice( 0, wordCount - 1 );
+
+  selectedWords.splice( Math.floor( wordCount / 2 ), 0, {
+    word: 'Free space'
+  });
+
+  return selectedWords;
 }
 
 // Variation of Paul Irish's log() for my IIFE setup:
